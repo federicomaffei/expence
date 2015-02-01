982e8f9b80f7eba2bc5ecc3bdee4160193d6ea16
federicomaffei <federico.maffei@gmail.com>
2015-02-01 14:13:54 +0000

    added mongodb connection

--- plugins/router.js
+++ plugins/router.js
@@ -1,4 +1,9 @@
-'use strict'
+'use strict';
+
+var authentication = require('../controllers/authentication');
+var database = require('../database')
+var joi = require('joi');
+var user = require('../models/user');
 
 exports.register = function (server, options, next) {
     server.route({
@@ -8,6 +13,55 @@ exports.register = function (server, options, next) {
             reply.view('index');
         }
     });
+
+    server.route({
+        method: 'GET',
+        path: '/login',
+        handler: function (request, reply) {
+            reply.view('login');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/register',
+        handler: function (request, reply) {
+            reply.view('register');
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/login',
+        handler: function (request, reply) {
+            reply('Hi your e-mail is "' + request.payload.email + '", that\'s all!');
+        },
+        config: {
+            validate: {
+                payload: {
+                    email: joi.string().email().required(),
+                    password: joi.string().required()
+                }
+            }
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/register',
+        handler: function (request, reply) {
+            reply('Hi your e-mail is "' + request.payload.email + '", that\'s all!');
+        },
+        config: {
+            validate: {
+                payload: {
+                    email: joi.string().email().required(),
+                    password: joi.string().required()
+                }
+            }
+        }
+    });
+
 };
 
 exports.register.attributes = {
