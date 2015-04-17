b2a8f312427192f9e3fd73dd05e96a3c57b5a4ec
federicomaffei <federico.maffei@gmail.com>
2015-04-18 00:35:35 +0100

    refactored calculateTotal function

--- src/lib/accounting.js
+++ src/lib/accounting.js
@@ -3,9 +3,9 @@
 module.exports = {
     calculateTotal: function(expenses, callback) {
         var total = 0;
-        for(var index = 0; index < expenses.length; index++) {
-            total = expenses[index].amount + total;
-        }
+        expenses.map(function(item){
+            total = item.amount + total;
+        });
         callback(total);
     }
 };
