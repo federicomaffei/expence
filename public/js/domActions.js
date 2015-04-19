6cb8172855ffdae8dffa20f1342515c24421d6e2
federicomaffei <federico.maffei@gmail.com>
2015-04-19 22:00:28 +0100

    added front-end to edit expense

--- public/js/domActions.js
+++ public/js/domActions.js
@@ -1,14 +1,47 @@
 $(document).ready(function(){
-    $('.delete-link').click(function(){
-        var id = this.dataset.id;
+    var deleteLink = $('.delete-link');
+    var deleteId = deleteLink.data('id');
+    deleteLink.click(function(){
         var newTotal = parseInt($('#expense-total').data('total')) - parseInt(this.dataset.amount);
         $.ajax({
-            url: '/expenses/' + id,
+            url: '/expenses/' + deleteId,
             type: 'DELETE',
             success: function() {
-                $('#expense-' + id).remove();
+                $('#expense-' + deleteId).remove();
                 $('#expense-total').text((newTotal/100).toFixed(2) + ' GBP');
             }
         });
     });
+
+    var editLink = $('.edit-link');
+    editLink.click(function(){
+        var editId = this.dataset.id;
+        var editLinkId = $('#edit-form-' + editId);
+        if(editLinkId.css('display') === 'none') {
+            editLinkId.show('fast');
+        } else {
+            editLinkId.hide('fast');
+        }
+    });
+
+    var editButton = $('.edit-button');
+    editButton.click(function(){
+        var editId = this.dataset.id;
+        $.ajax({
+            type: "PUT",
+            contentType: "application/json; charset=utf-8",
+            url: '/expenses/' + editId,
+            data: JSON.stringify({
+
+            }),
+            dataType: "json",
+            success: function (msg) {
+                console.log(msg);
+            },
+            error: function (err){
+                console.log(err);
+            }
+        });
+    });
+
 });
